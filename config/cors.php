515c2c702d1db0562313cabfa54fa5a117d3e3fe
hungnt <nthung.nvit.yenbinh@gmail.com>
2025-05-08 10:53:24 +0700

    fix_08_05

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,7 @@ return [
     'allowed_methods' => ['*'],
 
     // 'allowed_origins' => ['https://yenbinh-web-app.vercel.app'],
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
